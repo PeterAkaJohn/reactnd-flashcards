f895f00bc2891026e0a6c2222a86b53ea0fb72cc
PierpaoloIannone <piannone@byte-code.com>
2017-12-09 17:07:06 +0100

    feat: setup api calls and helper methods

---
+++ utils/api.js
@@ -0,0 +1,46 @@
+import { AsyncStorage } from "react-native";
+import { getDefaultDeckObject, startingState } from "./helper";
+
+const STORAGE_KEY = "reactnd-flashcards";
+
+function initializeStorage() {
+  return AsyncStorage.setItem(STORAGE_KEY, startingState).then(function() {
+    return startingState;
+  });
+}
+
+export function getDecksFromStorage() {
+  return AsyncStorage.getItem(STORAGE_KEY).then(data => {
+    if (!data) {
+      //no data available load starting data
+      return initializeStorage();
+    }
+    return JSON.parse(data);
+  });
+}
+
+export function getDeck(deckId) {
+  return AsyncStorage.getItem(STORAGE_KEY).then(data => {
+    console.log(data);
+    return JSON.parse(data[deckId]);
+  });
+}
+
+export function saveDeckTitle(deckName) {
+  return AsyncStorage.mergeItem(
+    STORAGE_KEY,
+    JSON.stringify({
+      [deckName]: getDefaultDeckObject(deckName)
+    })
+  );
+}
+
+// card is made up of question and answer strings
+export function addCardToDeck(deckId, card) {
+  return AsyncStorage.mergeItem(
+    STORAGE_KEY,
+    JSON.stringify({
+      [deckId]: [...questions, card]
+    })
+  );
+}
