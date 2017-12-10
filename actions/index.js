da78e38549de4dd907960565918b335560efb0cf
PierpaoloIannone <piannone@byte-code.com>
2017-12-10 16:03:06 +0100

    feat: handle new deck in actions and reducers

--- actions/index.js
+++ actions/index.js
@@ -1,8 +1,8 @@
-import { getDecksFromStorage } from "../utils/api";
+import { getDecksFromStorage, saveDeckTitle } from "../utils/api";
 export const GET_DECKS = "GET_DECKS";
+export const NEW_DECK = "NEW_DECK";
 
 function onSuccessGetDecks(data) {
-  console.log(data);
   return {
     type: GET_DECKS,
     payload: data
@@ -14,6 +14,25 @@ export function getDecks() {
     return getDecksFromStorage().then(
       data => {
         dispatch(onSuccessGetDecks(data));
+      },
+      error => console.log(error)
+    );
+  };
+}
+
+function onSuccessNewDeck(data) {
+  return {
+    type: NEW_DECK,
+    payload: data
+  };
+}
+
+export function insertNewDeck(deckName) {
+  return function(dispatch) {
+    return saveDeckTitle(deckName).then(
+      data => {
+        dispatch(onSuccessNewDeck(data));
+        return data;
       },
       error => console.log(error)
     );
--- reducers/index.js
+++ reducers/index.js
@@ -1,4 +1,4 @@
-import { GET_DECKS } from "../actions";
+import { GET_DECKS, NEW_DECK } from "../actions";
 
 import { getStartingState } from "../utils/helper";
 
@@ -6,6 +6,11 @@ export function decksReducer(state = {}, action) {
   switch (action.type) {
     case GET_DECKS:
       return action.payload;
+    case NEW_DECK:
+      return {
+        ...state,
+        [action.payload.title]: action.payload
+      };
     default:
       return state;
   }
