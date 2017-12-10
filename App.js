19eb78d8d716f4a55e85fdafe0ffbcb50303c4cd
PierpaoloIannone <piannone@byte-code.com>
2017-12-10 16:02:50 +0100

    feat: Implement status bar and navigation

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Platform } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./utils/store";
-import Decks from "./components/decks/Decks";
+import MainNavigator from "./components/common/MainNavigator";
+import CustomStatusBar from "./components/common/CustomStatusBar";
+import { blue } from "./utils/colors";
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <View style={styles.container}>
-          <Decks />
+          <CustomStatusBar backgroundColor={blue} barStyle="light-content" />
+          <MainNavigator />
         </View>
       </Provider>
     );
@@ -19,9 +22,7 @@ class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: blue
   }
 });
 
---
+++ components/common/MainNavigator.js
@@ -0,0 +1,87 @@
+import React from "react";
+import { TabNavigator, StackNavigator } from "react-navigation";
+import { Platform } from "react-native";
+import { blue, purple, white } from "../../utils/colors";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import Decks from "../decks/Decks";
+import AddCard from "../addCard/AddCard";
+import DeckDetail from "../deckDetail/DeckDetail";
+import Quiz from "../quiz/Quiz";
+import NewDeck from "../newdeck/NewDeck";
+
+const Tabs = TabNavigator(
+  {
+    Decks: {
+      screen: Decks,
+      navigationOptions: {
+        tabBarLabel: "Decks",
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-bookmarks" size={30} color={tintColor} />
+        )
+      }
+    },
+    NewDeck: {
+      screen: NewDeck,
+      navigationOptions: {
+        tabBarLabel: "New Deck",
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-bookmarks" size={30} color={tintColor} />
+        )
+      }
+    }
+  },
+  {
+    navigationOptions: {
+      header: null
+    },
+    tabBarOptions: {
+      activeTintColor: Platform.OS === "ios" ? purple : white,
+      style: {
+        height: 56,
+        backgroundColor: Platform.OS === "ios" ? white : purple,
+        shadowColor: "rgba(0, 0, 0, 0.24)",
+        shadowOffset: {
+          width: 0,
+          height: 3
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1
+      }
+    }
+  }
+);
+
+const MainNavigator = StackNavigator({
+  Home: {
+    screen: Tabs
+  },
+  DeckDetail: {
+    screen: DeckDetail,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple
+      }
+    }
+  },
+  AddCard: {
+    screen: AddCard,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple
+      }
+    }
+  },
+  Quiz: {
+    screen: Quiz,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple
+      }
+    }
+  }
+});
+
+export default MainNavigator;
