e01658883bd45e9a6ec26e3a33e2afd64b2fde58
PierpaoloIannone <piannone@byte-code.com>
2017-12-26 09:50:01 +0100

    feat: add colors to colors.js

--- utils/colors.js
+++ utils/colors.js
@@ -6,3 +6,11 @@ export const orange = "#f26f28";
 export const blue = "#4e4cb8";
 export const lightPurp = "#7c53c3";
 export const pink = "#b93fb3";
+
+export const greenLight = "#00E676";
+export const redLight = "#FF3D00";
+export const blueBase = "#2196F3";
+export const primaryText = "#E0F2F1";
+export const purpleLight = "#E040FB";
+export const pinkLight = "#FF4081";
+export const yellowBase = "#FFEB3B";
